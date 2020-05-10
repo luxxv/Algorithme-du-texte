a8e4b06781140b4bd1e7062877f83963feebdbe3
AmineYahouni <59349174+AmineYahouni@users.noreply.github.com>
2020-05-10 18:16:47 +0200

    Update methode.py
    J'ai indiqué en commentaire les méthode que j'ai validé

--- methode.py
+++ methode.py
@@ -7,6 +7,7 @@ import indexe_inverse
 # Charger tous les fichiers que le répertoire contient, quel que soit le mode d'encodage du fichier.
 monRepertoire = "./pages_web/"
 
+#***********************Valider par Amine Yahouni************************************ 
 #Retourne le nombre de mots dans un document choisi, donc sa longueur
 def longueurDoc(document):
     doc = open(document,'r') #ouverture et lecture du fichier choisi
@@ -16,6 +17,8 @@ def longueurDoc(document):
     document.close() #fermeture du fichier
     return nbtokendoc
 
+
+#***********************Valider par Amine Yahouni************************************ 
 #Average Document Length,  la longueur moyenne des documents de la collection
 def avgdl(monRepertoire):                                      #A FINIR
     fichiers = [f for f in listdir(monRepertoire)] # les fichiers du répertoire sont chargés
@@ -36,7 +39,9 @@ def avgdl(monRepertoire):                                      #A FINIR
     #word = rd.split(" ")
     #average = sum(len(word) for word in words) / len(words)
     #return average
+    
 
+#***********************Valider par Amine Yahouni************************************ 
 def ftd(mot, document):
     doc = open(document,'r') #ouverture et lecture du fichier choisi
     rd = file.read() #chargement du fichier dan sune variable
@@ -45,6 +50,9 @@ def ftd(mot, document):
     document.close()
     return nbterme
 
+
+
+#***********************Valider par Amine Yahouni************************************ 
 def chargerIndex(monRepertoire):
     fichiers = [f for f in listdir(monRepertoire)] # les fichiers du répertoire sont chargés
     # print(fichiers) #L'affichage est optionnel
@@ -52,6 +60,8 @@ def chargerIndex(monRepertoire):
     number_files = len(list) # fonction len qui va retourner un entier qui sera la longueur de la liste
     return number_files #renvoie nombre de fichiers
 
+
+#***********************Valider par Amine Yahouni************************************ 
 #Les fonction capables de calculer les variantes d'un TF-IDF
 #TERM FREQUENCY TF = nb(t∈d)/nb(d)
 #terme="Lyon" #terme que nous voulons compter dans un fichier
@@ -65,6 +75,8 @@ def tf(mot, document, monRepertoire):
     TF = nbterme / nbtokendoc #ce qui nous mène à coder la formule associé à Term Frequency
     return TF
 
+
+#***********************Valider par Amine Yahouni************************************ 
 #INVERSE DOCUMENT FREQUENCY IDF; mot et une collection de documents
 def idf(mot, document, monRepertoire):
     #doc = open(document,'r')
@@ -80,26 +92,42 @@ def idf(mot, document, monRepertoire):
         IDF = math.log(val)
         return IDF #renvoie valeur de IDF
 
+    
+
+    
+    
+#***********************Valider par Amine Yahouni************************************ 
 #Normalisation Logarithmique
 def N_Log(mot, document):
     res = 1+math.log(ftd(mot, document))
     return res
 
+
+
+#***********************Valider par Amine Yahouni************************************ 
 #Normalisation par le max
 def N_Max(mot, document,terme):
     sol=0.5+0.5*(ftd(mot, document)/max(ftd(terme, document)))
     return sol
 
+
+
+#***********************Valider par Amine Yahouni************************************ 
 #Formule TF-IDF
 def TF_IDF(mot, document, monRepertoire):
     tfidf=tf(mot, document, monRepertoire)*idf(mot, document, monRepertoire)
     return tfidf
 
+
+
+#***********************Valider par Amine Yahouni************************************ 
 def BM_25(mot, document, monRepertoire,k1):
     b=0.75
     resultat = idf(mot, document, monRepertoire)*((ftd(mot, document)*(k1+1))/(ftd(mot, document)+(k1*(1-b+(b*longueurDoc(document)/avgdl(monRepertoire))))))
 
+    
 
+#***********************Valider par Amine Yahouni************************************ 
 #une fonction recherche qui prend en entrée séquence de mots-clés et qui renvoie les 10 premières pages qui maximisent le score pour ces mots clés
 def recherche(mots):
     c=0 #on initialise le compteur
