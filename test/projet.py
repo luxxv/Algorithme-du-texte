c21467eb2b80f5ca155c6ca656424dac7088cdf7
AmineYahouni <59349174+AmineYahouni@users.noreply.github.com>
2020-05-10 18:52:51 +0200

    Update projet.py
    Je t'ai proposé un block bien commenter pour arranger les bug présent

--- test/projet.py
+++ test/projet.py
@@ -1,7 +1,8 @@
 from os import listdir #importation bibliothèque lecture fichier et répertoire
 import os
 import math
-import indexe_inverse
+#import indexe_inverse # je ne vois pas l'utilité de cet import ??
+import re
 # Charger les documents dans l'index
 # La méthode doit parcourir les documents d'un répertoire passer en entrée.
 # Charger tous les fichiers que le répertoire contient, quel que soit le mode d'encodage du fichier.
@@ -17,12 +18,52 @@ print("vérif chargement",number_files) #affichage
 terme="Lyon" #terme que nous voulons compter dans un fichier
 file = open('./pages_web/lipn.fr_man-www2018_MAN%40WWW2018_Home.html','r') #ouverture et lecture du fichier choisi
 rd = file.read() #chargement du fichier dansune variable
-liste = rd.split(" ") #chaque token est chargé dans une liste
-print(liste) #affichage pour vérifier
-print(liste.count(terme)) #affichage du nombre de termes equivalent à celui entré en paramètre
-nbtokendoc = len(liste)
+
+#****************Block modifié pour cause de dysfonctionnement de la méthode employée************************ 
+#liste = rd.split(" ") #chaque token est chargé dans une liste
+#print(liste) #affichage pour vérifier
+#print(liste.count(terme)) #affichage du nombre de termes equivalent à celui entré en paramètre
+#nbterme = liste.count(terme) #variable qui va stocker l'entier correspondant à l'équivalence
+#nbtokendoc = len(liste)
+#print("LONGUEUR DOCUMENT :",nbtokendoc) #affichage
+
+#***************Block proposer par Amine Yahouni*********************
+
+def isprefixe(texte,mot,i):
+        """Vérifie si mot a une occurrence dans texte en position i"""
+        B = True
+        j = 0
+        while (j < len(mot)) and B:
+            if texte[i+j] != mot[j]:
+                B = False
+            j +=1
+        return B
+    
+def cherche_occurrences(texte, mot):
+        """Donne la liste de toutes les occurrences de mot dans texte"""
+        occ = [] # liste des occurrences
+        for i in range(len(texte)-len(mot)+1):
+            if isprefixe(texte,mot,i):
+                occ.append(i)
+        return occ
+    
+
+#Par la suite l'idée est de rechercher les occurence d'un mot donné 
+nbterme=len(cherche_occureences(rd,terme)) #variable qui va stocker l'entier correspondant à l'équivalence
+print("Occurence du terme :'",terme,"' dans notre Texte est :",nbterme)#Affichage
+
+#pour les nbtokendoc nous allons utlisé regex pour compter le nombre de mot avec findall
+nbtokendoc = len(re.findall(r'\w+', rd)) #le nombre total des mots de notre fichier
 print("LONGUEUR DOCUMENT :",nbtokendoc) #affichage
-nbterme = liste.count(terme) #variable qui va stocker l'entier correspondant à l'équivalence
+
+#on peut aussi afficher la liste des mots si nécessaire 
+#print(re.findall(r'\w+', rd))
+
+#*******************************Fin du block proposer par Amine Yahouni**********************************
+
+
+
+
 #average = sum(len(liste) for liste in words) / len(liste)
 #print("AVERAGE =", average)
 #print(rd)
